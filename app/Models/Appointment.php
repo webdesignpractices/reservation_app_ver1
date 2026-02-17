07109d236ba4212a40406b76d836f47859140ddf
ar <officebusiness800@gmail.com>
2026-02-17 18:00:21 +0900

    作成したモデルにマスアサイメントのためのfillableを定義した。そしてAppointment.phpにリレーション設定をした

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Staff;
 class Appointment extends Model
 {
-    //
+
+    protected $fillable = [
+        'user_id',
+        'staff_id',
+        'service_id',
+        'start_at',
+        'end_at',
+        'memo',
+        'status',
+    ];
+    //$appointment->user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    //$appointment->service
+    public function service(){
+        return $this->belongsTo(Service::class);
+    }
+    //$appointment->staff
+    public function staff(){
+        return $this->belongsTo(Staff::class);
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'duration_minutes',
+        'price',
+    ];
 }
