19ae4a12ede2fa37aacc2699fae37dde448e30a4
ar <officebusiness800@gmail.com>
2026-08-08 13:36:14 +0900

    ユーザーのマイページから現在の予約状況を取得するためには中間テーブルが必要だと気付いたので作成する。

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Appointment;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -22,6 +23,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    //$user->appointments
+    public function appointments(){
+        return $this->belongsToMany(Appointment::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -5,3 +5,12 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
     <title>マイページ</title>
 </head>
+<div class="container">
+    <div class="main"> 
+        <h1>↓予約一覧↓</h1>
+        <p>{{}}</p>
+
+    </div>  
+</div>
+</body>
+</html>
